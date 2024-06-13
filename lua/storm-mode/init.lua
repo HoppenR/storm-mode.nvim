5e223c977ffd283db74f77d93307bf01642cbf62
Christoffer Lundell <christofferlundell@protonmail.com>
2024-06-13 14:15:12 +0200

    feat(handlers): implement base for handling msgs

--- lua/storm-mode/init.lua
+++ lua/storm-mode/init.lua
@@ -6,15 +6,15 @@ function M.setup(opts)
     require('storm-mode.config').setup(opts)
 end
 
--- TODO: Add more events, add more filetypes
+-- TODO(Hop): Add more events, add more filetypes
 --       can ask the LSP which filetype is good
 
--- TODO: Fix edge case where an entire message is left in the buffer
---       without being processed
+-- TODO(Hop): Fix edge case where an entire message is left in the buffer
+--       without being processed, right after another one is.
 
 -- TODO(Hop): Keep track of edit points
 --            buffer.lua: { storm_buffer_last_point, cursor_position }
--- TODO(Hop): Fix handlers for LSP message types (sym 'color', ...)
--- TODO(Hop): handle waiting for LSP to start up
+
+-- TODO(Hop): Implement handlers for LSP message types (sym 'color', ...)
 
 return M
